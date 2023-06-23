91eae21165f26091138cfe7c2a311d17ee947d20
Diana Smilian <dianka.kulakevich@gmail.com>
2023-06-23 12:17:13 +0300

    stop train

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
+// import Form from './components/Form/Form';
 // import Counter from './components/Counter';
 // import Dropdown from './components/Dropdown/Dropdown';
 // import ColorPicker from './components/ColorPicker/ColorPicker';
@@ -24,16 +25,36 @@ class App extends Component {
     ],
   }
 
+  formSubmitHandler = data => {
+   console.log(data)
+  }
+
   deleteTodo = todoId => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== todoId),
     }))
   }
+
+  toggleCompleted = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+            if (todo.id === todoId) {
+              return {
+                ...todo,
+                completed: !todo.completed,
+              };
+            }
+    
+            return todo;
+    })
+  }))};
+
   render() {
     const {todos} = this.state;
     const completedTodosCount = todos.reduce((acc, todo) => (todo.completed ? acc + 1 : acc), 0);
     return (
       <>
+      {/* <Form onSubmit={this.formSubmitHandler}/> */}
   {/* <Dropdown />  */}
   {/* <ColorPicker options={colorPickerOptions}/>
   <Counter initialValue={10}/> */}
@@ -42,7 +63,11 @@ class App extends Component {
     <p>К-сть виконаних: {completedTodosCount}</p>
   </div>
 
-  <TodoList todos={todos} onDeleteTodo={this.deleteTodo}/>
+  <TodoList 
+  todos={todos} 
+  onDeleteTodo={this.deleteTodo}
+  onToggleCompleted={this.toggleCompleted}
+  />
  </>
     );
   }
